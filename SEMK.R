04ecaceea87d9b94d97a8e3ad7be24706405ec3a
Carles Boix <carles.adsera@gmail.com>
2013-07-08 17:16:08 -0400

    Create central makeall file and account for multiple SNPs in the same site in createbatch

--- SEMK.R
+++ SEMK.R
@@ -1,27 +1,34 @@
 #!/usr/bin/R
 
 SEMK <- function(x,data){
-
     v <- c(unlist(data[x,2:5]))
     Ds <- v[1]
     Dn <- v[2]
     Ps <- v[3]
     Pn <- v[4] 
-    scalefactor <- (Pn + Ps)/(Dn + Ds)
-    Dn <- scalefactor * Dn
-    Ds <- scalefactor * Ds
     alpha <- 1 - (Ds/Dn)*(Pn/Ps) 
-    return(alpha)
+    return(c(alpha,Ds,Dn,Ps,Pn))
 }
 
 # Read and Remove bad data points:
 read.delim('~/project/divergence/fulldiv',header=FALSE,sep=" ") -> tab
+#read.delim('~/Labwork/Rwork/Divall',header=FALSE,sep=" ") -> tab
 for(i in 2:5){tab <- tab[!tab[,i] == 0,]}
 
 #Run MK function and write file
 out <- sapply(1:nrow(tab),data=tab,SEMK)
-out <- data.frame(out)
+out <- data.frame(t(out))
 out <- cbind(tab$V1,out)
+names(out) <- c('Gene','alpha','Ds','Dn','Ps','Pn')
+Average <- with(out, mean(Ds)/mean(Dn)*mean(Pn/(Ps + 1)))
 
+#Write the out data (genes/alpha values)
 write.table(out,file="~/project/SEMKdivsim",sep=" ")
 
+#Create figure and send to png
+library(ggplot2)
+g1 <- ggplot(out,aes(Gene,alpha)) + geom_point(pch=1) + geom_hline(yintercept=Average,color='red',linetype='dotted') + theme_bw() + theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+png('~/project/output/SEMK.png')
+g1
+dev.off()
