2aaca5da8eddb6bf8cd01e6d41f2071db5e7ccb2
Carles Boix <carles.adsera@gmail.com>
2013-07-10 18:28:56 -0400

    After a revert to get rid of 20k lines of useless batchfiles. Added the new parsing files.

--- code/aminocomp.py
+++ code/aminocomp.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python2.6
+PREFIX='/net/home/carlesba' #Take from arguments in bash.
+CDorNC='cd' #another argument that can be taken in bash.
+REFRESH=False #canonically false, but can be overwritten. THIS whole section should be parsed as args.
+
+import os.path
+
 # Be sure to run w/ python2.6 (v2.6.5) instead of python (which is python v2.4.3)
-# A sample of the code found in aacomp.py: 
 bases = ['t', 'c', 'a', 'g']
 codons = [a+b+c for a in bases for b in bases for c in bases]
 amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
@@ -19,11 +24,25 @@ def compacids(a,b):
         else:
             return "NS"
 
-#NOTE Touch the output files each time (rm in fact)
-with open('/net/home/carlesba/project/divergence/Akt1.simdiv','r') as f:
-    with open('/net/home/carlesba/project/divergence/Akt1.out','a') as out:
-        for line in f:
-            a = line.split(" ")
-            a[-1] = a[-1][0]
-            code = " ".join(a) + " " + compacids(a[0],a[1]) + "\n"
-            out.write(code)
+# Function to write the comparison for one gene
+def genecomp(gene,prefix,cdnc,refresh):
+    namef = prefix + '/project/divergence/' + gene + '.' + cdnc + '.simdiv'
+    nameout = prefix + '/project/divergence/' + gene + '.' + cdnc + '.out'
+
+	# If out file exists, don't do this unless refresh.
+    if (not os.path.exists(nameout) or refresh):
+		with open(namef,'r') as f:
+		    with open(nameout,'a') as out:
+		        for line in f:
+		            a = line.split(" ")
+		            a[-1] = a[-1][0]
+		            code = " ".join(a) + " " + compacids(a[0],a[1]) + "\n"
+		            out.write(code)
+
+# Loop over all gene targets
+targets= PREFIX + '/project/dmel_targets'
+with open(targets,'r') as tar:
+    for geneline in tar:
+        g = geneline.split(" ")
+        genecomp(g[0],PREFIX,'cd',REFRESH)
+        genecomp(g[0],PREFIX,'nc',REFRESH)
